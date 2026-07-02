712d72ecffa88b17e64197976bb873ceb941de7c
Jeremy Ellis <jeremy@ellisnet.com>
2026-07-01 23:52:13 -0700

    Work in progress on alternate Platforms plus samples that aren't CodeBrix.Platform targets.

---
+++ samples/Platforms/JustBetweenUs/Shared/ViewModels/EncryptionMode.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+using CodeBrix.Platform.Simple;
+
+// ReSharper disable once CheckNamespace
+namespace JustBetweenUs.ViewModels;
+
+public class EncryptionMode : SimpleEnumInfo<EncryptionMode.CryptAlgorithm>
+{
+    public enum CryptAlgorithm
+    {
+        [SimpleEnum<EncryptionMode>(nameof(EncryptionMode.Aes))]
+        Aes = 0,
+
+        [SimpleEnum<EncryptionMode>(nameof(EncryptionMode.TripleDes))]
+        TripleDes,
+
+        [SimpleEnum<EncryptionMode>(nameof(EncryptionMode.Twofish))]
+        Twofish,
+    }
+
+    public static EncryptionMode Aes => new(CryptAlgorithm.Aes,
+        "AES Standard Encryption (Secure)");
+
+    public static EncryptionMode TripleDes => new(CryptAlgorithm.TripleDes,
+        "Triple DES (Obsolete, insecure)");
+
+    public static EncryptionMode Twofish => new(CryptAlgorithm.Twofish,
+        "Twofish Encryption (Very secure)");
+
+    public EncryptionMode(CryptAlgorithm algorithm, string description)
+        : base(algorithm) =>
+        Description = description?.Trim();
+
+    public static Dictionary<CryptAlgorithm, EncryptionMode> GetDictionary() =>
+        GetDictionary<EncryptionMode>();
+}
---
+++ src-platforms/Platform.WinUI/Skia/Controls/ImagePosition.cs
@@ -0,0 +1,15 @@
+#pragma warning disable IDE0130
+
+namespace CodeBrix.Platform.WinUI.Controls;
+
+/// <summary>
+/// Specifies the position of the image relative to the text
+/// in an <see cref="EmbeddedImageButton"/>.
+/// </summary>
+public enum ImagePosition
+{
+    Left,
+    Top,
+    Right,
+    Bottom
+}
