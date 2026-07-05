19b92d756b009e4d605971430f38cbf3b8c487be
Jeremy Ellis <jeremy@ellisnet.com>
2026-07-04 22:34:06 -0700

    Package version updates, fix for WebView on Windows.

--- src/SourceGenerators/Platform.UI.SourceGenerators.Tests/XamlCodeGeneratorTests/Given_Parser.cs
+++ src/SourceGenerators/Platform.UI.SourceGenerators.Tests/XamlCodeGeneratorTests/Given_Parser.cs
@@ -843,7 +843,7 @@ public partial class Given_Parser
 					"""
 				}
 			},
-			ReferenceAssemblies = _Dotnet.Current.WithCodeBrixPackage("1.0.183.233"),
+			ReferenceAssemblies = _Dotnet.Current.WithCodeBrixPackage("1.0.185.1199"),
 		}.AddGeneratedSources();
 
 		await test.RunAsync();
