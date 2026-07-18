442d778718ae03c57f7ddee954b0226095213747
Jeremy Ellis <jeremy@ellisnet.com>
2026-07-18 08:11:30 -0700

    Bumped nuget versions.

--- src/SourceGenerators/Platform.UI.SourceGenerators.Tests/XamlCodeGeneratorTests/Given_Parser.cs
+++ src/SourceGenerators/Platform.UI.SourceGenerators.Tests/XamlCodeGeneratorTests/Given_Parser.cs
@@ -843,7 +843,7 @@ public partial class Given_Parser
 					"""
 				}
 			},
-			ReferenceAssemblies = _Dotnet.Current.WithCodeBrixPackage("1.0.197.800"),
+			ReferenceAssemblies = _Dotnet.Current.WithCodeBrixPackage("1.0.199.897"),
 		}.AddGeneratedSources();
 
 		await test.RunAsync();
