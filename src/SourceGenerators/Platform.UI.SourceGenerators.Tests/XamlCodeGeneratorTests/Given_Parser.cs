48f94e8f4dd8bee9cc56306f179fb87896b78a6e
Jeremy Ellis <jeremy@ellisnet.com>
2026-08-30 22:44:53 -0700

    CodeBrix.Platform nuget package bumps.

--- src/SourceGenerators/Platform.UI.SourceGenerators.Tests/XamlCodeGeneratorTests/Given_Parser.cs
+++ src/SourceGenerators/Platform.UI.SourceGenerators.Tests/XamlCodeGeneratorTests/Given_Parser.cs
@@ -843,7 +843,7 @@ public partial class Given_Parser
 					"""
 				}
 			},
-			ReferenceAssemblies = _Dotnet.Current.WithCodeBrixPackage("1.0.231.233"),
+			ReferenceAssemblies = _Dotnet.Current.WithCodeBrixPackage("1.0.243.226"),
 		}.AddGeneratedSources();
 
 		await test.RunAsync();
