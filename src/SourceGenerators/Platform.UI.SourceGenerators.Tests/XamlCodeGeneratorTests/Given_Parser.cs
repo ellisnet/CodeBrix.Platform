839e2e5c54386af912d6e0f2d7475d2fd494828a
Jeremy Ellis <jeremy@ellisnet.com>
2026-09-10 20:27:25 -0700

    Nuget package bumps.

--- src/SourceGenerators/Platform.UI.SourceGenerators.Tests/XamlCodeGeneratorTests/Given_Parser.cs
+++ src/SourceGenerators/Platform.UI.SourceGenerators.Tests/XamlCodeGeneratorTests/Given_Parser.cs
@@ -796,7 +796,7 @@ public partial class Given_Parser
 					"""
 				}
 			},
-			ReferenceAssemblies = _Dotnet.Current.ReferenceAssemblies.AddPackages([new PackageIdentity("CodeBrix.Platform.SkiaSharp.Views.MitLicenseForever", "4.151.0")]),
+			ReferenceAssemblies = _Dotnet.Current.ReferenceAssemblies.AddPackages([new PackageIdentity("CodeBrix.Platform.SkiaSharp.Views.MitLicenseForever", "4.152.0")]),
 		}.AddGeneratedSources();
 
 		await test.RunAsync();
@@ -843,7 +843,7 @@ public partial class Given_Parser
 					"""
 				}
 			},
-			ReferenceAssemblies = _Dotnet.Current.WithCodeBrixPackage("1.0.243.226"),
+			ReferenceAssemblies = _Dotnet.Current.WithCodeBrixPackage("1.0.254.167"),
 		}.AddGeneratedSources();
 
 		await test.RunAsync();
